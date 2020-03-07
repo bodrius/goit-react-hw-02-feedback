0b3b9515df0f5cb202652ae2e1c543e061bfbe95
Bogdan Pechenyk <bogdanpechenyk@gmail.com>
2020-03-07 21:07:21 +0200

    create step 1-2

--- task-2/src/component/blockButton/BlockButton.js
+++ task-2/src/component/blockButton/BlockButton.js
@@ -1,19 +1,19 @@
-import React from 'react'
+// import React from 'react'
 
-const BlockButton = () => {
-    return (
-        <>
-        <button onClick={this.onHandleClickButton} name="good">
-          Good
-        </button>
-        <button onClick={this.onHandleClickButton} name="neutral">
-          Neutral
-        </button>
-        <button onClick={this.onHandleClickButton} name="bad">
-          Bad
-        </button>
-        </>
-    );
-}
+// const BlockButton = () => {
+//     return (
+//         <>
+//         <button onClick={this.onHandleClickButton} name="good">
+//           Good
+//         </button>
+//         <button onClick={this.onHandleClickButton} name="neutral">
+//           Neutral
+//         </button>
+//         <button onClick={this.onHandleClickButton} name="bad">
+//           Bad
+//         </button>
+//         </>
+//     );
+// }
 
-export default BlockButton;
+// export default BlockButton;
--- task-2/src/component/statistic/Statistic.js
+++ task-2/src/component/statistic/Statistic.js
@@ -3,15 +3,13 @@ import css from "./statistic.module.css";
 // import ShowStatistic from '../showStatistics/ShowStatistics';
 // import BlockButton from '../blockButton/BlockButton'
 
-const initialState={
-    good: 0,
-    neutral: 0,
-    bad: 0,
-    total:0,
-    positivePercentage:0
-}
-
-
+const initialState = {
+  good: 0,
+  neutral: 0,
+  bad: 0,
+  total: 0,
+  positivePercentage: 0
+};
 class Statistic extends Component {
   state = {
     ...initialState
@@ -34,19 +32,18 @@ class Statistic extends Component {
 
   render() {
     return (
-      <>
+    
         <div className={css.container}>
-        <h1>Please leave feedback</h1>
-        <button onClick={this.onHandleClickButton} name="good">
-          Good
-        </button>
-        <button onClick={this.onHandleClickButton} name="neutral">
-          Neutral
-        </button>
-        <button onClick={this.onHandleClickButton} name="bad">
-          Bad
-        </button>
-       
+          <h1>Please leave feedback</h1>
+          <button onClick={this.onHandleClickButton} name="good">
+            Good
+          </button>
+          <button onClick={this.onHandleClickButton} name="neutral">
+            Neutral
+          </button>
+          <button onClick={this.onHandleClickButton} name="bad">
+            Bad
+          </button>
           <h3>Statistics</h3>
           <p>Good:{this.state.good}</p>
           <p>Neutral:{this.state.neutral}</p>
@@ -54,7 +51,7 @@ class Statistic extends Component {
           <h4>TOTAL: {this.countTotalFeedback()}</h4>
           <h4>Positive Feedback:{this.countPositiveFeedbackPercentage()} %</h4>
         </div>
-      </>
+      
     );
   }
 }
